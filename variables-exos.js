1bd759914e04b8d72884f108aeffb6c001adf365
seb3337 <sebast3337@gmail.com>
2024-11-28 09:32:28 +0100

    new file

---
+++ variables-exos.js
@@ -0,0 +1,19 @@
+ /**
+   * -------------------------------------------------------
+   * 1 - Renommer les variables correctement
+   * -------------------------------------------------------
+*/
+
+const firstname = "Bruno";
+
+const message = "Salut je m'appelle Bruno et voici ma description";
+
+const job = "Analyste financier au Yémen";
+
+const age = 23;
+
+const town = "Bordeaux";
+
+const list = "Une liste de nombres";
+
+const firstname = "le prénom du plus grand agent secret de tous les temps";
